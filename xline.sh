5b62ba720811a97752d066b99154d125ea2855e8
kazuhikoh <kazuhiko.hrmt@gmail.com>
2019-03-24 12:54:42 +0900

    change compact output (contains blog's title & url)

--- xline.sh
+++ xline.sh
@@ -22,7 +22,7 @@ shift $((OPTIND - 1))
 readonly TIMELINE_POST='https://timeline.line.me/post/'
 readonly XPATH_INIT_DATA='//*[@id="init_data"]/text()'
 readonly JQ_POSTS='.userHome.feeds[].post'
-readonly JQ_POSTS_COMPACT=' | {post: (.postInfo.homeId + "/" + .postInfo.postId), text:.contents.text, title:.additionalContents.title}'
+readonly JQ_POSTS_COMPACT=' | {post: (.postInfo.homeId + "/" + .postInfo.postId), text:.contents.text, blogTitle:.additionalContents.title, blogUrl:.additionalContents.url.targetUrl}'
 
 cat - \
   | xmllint --xpath "$XPATH_INIT_DATA" --html - 2>/dev/null \
