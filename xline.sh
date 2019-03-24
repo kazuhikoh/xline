1287c728c99ab20415911cb5fe1432f7a89a0564
kazuhikoh <kazuhiko.hrmt@gmail.com>
2019-03-24 14:46:35 +0900

    formatting

--- xline.sh
+++ xline.sh
@@ -26,7 +26,7 @@ shift $((OPTIND - 1))
 readonly TIMELINE_POST='https://timeline.line.me/post/'
 readonly XPATH_INIT_DATA='//*[@id="init_data"]/text()'
 readonly JQ_POSTS='.userHome.feeds[].post'
-readonly JQ_POSTS_EXCLUDE_BLOG=' | select(.additionalContents ==null)'
+readonly JQ_POSTS_EXCLUDE_BLOG=' | select(.additionalContents == null)'
 readonly JQ_POSTS_COMPACT=' | {post: (.postInfo.homeId + "/" + .postInfo.postId), text:.contents.text, blogTitle:.additionalContents.title, blogUrl:.additionalContents.url.targetUrl}'
 
 cat - \
